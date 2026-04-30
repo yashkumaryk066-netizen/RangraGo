7376a6400489f4041c2c9223ce5ae870df93294c
yash kumar <yashkumaryk066@gamil.com>
2026-04-30 15:30:19 +0530

    updates

--- frontend/android/build.gradle.kts
+++ frontend/android/build.gradle.kts
@@ -5,29 +5,6 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    if (project.name != "app") {
-        afterEvaluate {
-            if (project.hasProperty("android")) {
-                val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
-                if (android.namespace == null) {
-                    android.namespace = "com.example.rangra_go.${project.name.replace("-", "_")}"
-                }
-            }
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
--- frontend/android/settings.gradle.kts
+++ frontend/android/settings.gradle.kts
@@ -18,10 +18,10 @@ pluginManagement {
 }
 
 plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 include(":app")
